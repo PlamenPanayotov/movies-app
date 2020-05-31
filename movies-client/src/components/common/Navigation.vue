e13b7cd7712c000d0c57f35adeb24a7d74c72d6e
PlamenPanayotov <p.panayotov@mail.bg>
2020-05-31 14:07:55 +0300

    home.vue

--- movies-client/src/components/common/Navigation.vue
+++ movies-client/src/components/common/Navigation.vue
@@ -1,16 +1,15 @@
 <template>
   <nav>
-      <router-link to="/login" exact>Login</router-link>
-      <router-link to="/register" exact>Register</router-link> 
+    <router-link to="/" exact>Home</router-link>
+    <router-link to="/login" exact>Login</router-link>
+    <router-link to="/register" exact>Register</router-link>
+    <router-link to="/profile" exact>Prolile</router-link>
   </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- movies-client/src/router.js
+++ movies-client/src/router.js
@@ -1,18 +1,34 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Register from './components/User/Register'
-import Login from './components/User/Login'
-import Profile from './components/User/Profile'
+import Home from './components/Home'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/register', component: Register },
-        { path: '/login', component: Login },
-        { path: '/profile', component: Profile }
+        {
+            path: '/',
+            component: Home,
+            meta: {
+                resources: {
+                    movies: 'https://api.themoviedb.org/3/movie/550?api_key=f5f5b5a829995b40eac24b23b82de87f'
+                }
+            }
+        },
+        {
+            path: '/register', component: () => import('./components/User/Register')
+        },
+        {
+            path: '/login', component: () => import('./components/User/Login')
+        },
+        {
+            path: '/profile', component: () => import('./components/User/Profile')
+        }
     ]
+});
+router.beforeEach((to, from, next) => {
+    
 });
 
 
